b2e0206ca85443b29206679044cc65c24581eaaf
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2018-03-10 21:21:04 +0000

    Draft new article

---
+++ blog/disabling-tls-v1/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!--Copyright Jamie Scaife-->
+<!--Legal Information at https://www.jamieweb.net/contact-->
+
+<head>
+    <title>Disabling TLSv1 and TLSv1.1</title>
+    <meta name="description" content="Disabling older TLS protocol versions.">
+    <meta name="keywords" content="Jamie, Scaife, jamie scaife, jamiescaife, jamieonubuntu, jamie90437, jamie90437x, jamieweb, jamieweb.net">
+    <meta name="author" content="Jamie Scaife">
+    <link href="jamie.css" rel="stylesheet">
+    <link href="https://www.jamieweb.net/blog/disabling-tlsv1-tlsv1.1/" rel="canonical">
+</head>
+
+<body>
+
+<?php include "navbar.php" ?>
+
+<div class="body">
+    <h1>Disabling TLSv1 and TLSv1.1</h1>
+    <hr>
+    <p><b>Saturday 10th March 2018</b></p>
+    <p></p>
+</div>
+
+<?php include "footer.php" ?>
+
+</body>
+
+</html>

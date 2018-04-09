87b263c04a8f9e685b0886b359e8db7963094f97
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2018-04-10 00:17:23 +0100

    Homepage edits

---
+++ index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!--Copyright Jamie Scaife-->
+<!--Legal Information at https://www.jamieweb.net/contact-->
+
+<head>
+    <title>JamieWeb</title>
+    <meta name="description" content="Website of Jamie Scaife - United Kingdom">
+    <meta name="keywords" content="Jamie, Scaife, jamie scaife, jamiescaife, jamieonubuntu, jamie90437, jamie90437x, jamieweb, jamieweb.net">
+    <meta name="author" content="Jamie Scaife">
+    <link href="/jamie.css" rel="stylesheet">
+    <link href="https://www.jamieweb.net/" rel="canonical">
+</head>
+
+<body>
+
+<?php include "navbar.php"; ?>
+
+<div class="body">
+    <div class="content redlink">
+        <h1>Jamie Scaife - United Kingdom</h1>
+        <hr>
+
+        <h3 class="no-mar-bottom intro-mar-top">No Ads, No Tracking, No JavaScript</h3>
+        <p class="two-mar-top">This website does not have any adverts, tracking or other internet annoyances.<br/>It's also 100% JavaScript free.</p>
+        <h3 class="no-mar-bottom">Tor Hidden Services</h3>
+        <p class="two-mar-top">This site is available through Tor at:</p>
+        <ul>
+            <li><p class="onionlink">Onion v2: <a class="two-mar-left" href="http://jamiewebgbelqfno.onion" target="_blank" rel="noopener">jamiewebgbelqfno.onion</a></p></li>
+            <li><p class="onionlink">Onion v3: <a class="two-mar-left" href="http://jamie3vkiwibfiwucd6vxijskbhpjdyajmzeor4mc4i7yopvpo4p7cyd.onion" target="_blank" rel="noopener">jamie3vkiwibfiwucd6vxijskbhpjdyajmzeor4mc4i7yopvpo4p7cyd.onion</a></p></li>
+        </ul>
+        <hr>
+
+        <h1 class="no-mar-bottom">Featured Content</h1>
+        <div class="featured">
+            <a href="/projects/computing-stats/">
+                <h3 class="no-mar-bottom">Raspberry Pi + BOINC Stats</h3>
+                <p class="two-no-mar"><b>Stats from my RPi cluster + BOINC.</b></p>
+                <p class="two-mar-top">Updated Every 10 Minutes</p>
+            </a>
+        </div>
+        <div class="featured">
+            <a href="/projects/block-exploitable-content/">
+                <h3 class="no-mar-bottom">Exploitable Web Content</h3>
+                <p class="two-no-mar"><b>Test whether exploitable web content is blocked in your web browser.</b></p>
+            </a>
+        </div>
+        <div class="featured">
+            <a href="/blog/namecoin-bit-domain/">
+                <h3 class="no-mar-bottom">Namecoin .bit Domain</h3>
+                <p class="two-no-mar"><b>Guide to registering a Namecoin .bit domain.</b></p>
+                <p class="two-mar-top">Tuesday 16th January 2017</p>
+            </a>
+        </div>
+        <div class="featured">
+            <a href="/blog/onionv3-hidden-service/">
+                <h3 class="no-mar-bottom">Tor Onion v3 Hidden Service</h3>
+                <p class="two-no-mar"><b>Testing the new Onion v3 Hidden Services.</b></p>
+                <p class="two-mar-top">Saturday 21st October 2017</p>
+            </a>
+        </div>
+        <hr>
+        <div class="display-flex">
+            <div class="menuitem">
+                <a href="/blog/">
+                    <h1>Blog</h1>
+                    <pre>BBBBBBBBBBBBBBBBB   
+B::::::::::::::::B  
+B::::::BBBBBB:::::B 
+BB:::::B     B:::::B
+  B::::B     B:::::B
+  B::::B     B:::::B
+  B::::BBBBBB:::::B 
+  B:::::::::::::BB  
+  B::::BBBBBB:::::B 
+  B::::B     B:::::B
+  B::::B     B:::::B
+  B::::B     B:::::B
+BB:::::BBBBBB::::::B
+B:::::::::::::::::B 
+B::::::::::::::::B  
+BBBBBBBBBBBBBBBBB   </pre>
+                </a>
+            </div>
+
+            <div class="menuitem flex-grow-1 padding-8-rl">
+                <a href="/projects/">
+                    <h1>Projects</h1>
+                    <pre>PPPPPPPPPPPPPPPPP   
+P::::::::::::::::P  
+P::::::PPPPPP:::::P 
+PP:::::P     P:::::P
+  P::::P     P:::::P
+  P::::P     P:::::P
+  P::::PPPPPP:::::P 
+  P:::::::::::::PP  
+  P::::PPPPPPPPP    
+  P::::P            
+  P::::P            
+  P::::P            
+PP::::::PP          
+P::::::::P          
+P::::::::P          
+PPPPPPPPPP          </pre>
+                </a>
+            </div>
+
+            <div class="menuitem">
+                <a href="/other/">
+                    <h1>Other</h1>
+                    <pre>     OOOOOOOOO     
+   OO:::::::::OO   
+ OO:::::::::::::OO 
+O:::::::OOO:::::::O
+O::::::O   O::::::O
+O:::::O     O:::::O
+O:::::O     O:::::O
+O:::::O     O:::::O
+O:::::O     O:::::O
+O:::::O     O:::::O
+O:::::O     O:::::O
+O::::::O   O::::::O
+O:::::::OOO:::::::O
+ OO:::::::::::::OO 
+   OO:::::::::OO   
+     OOOOOOOOO     </pre>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="sidebar">
+        <input type="radio" class="gravityradio" id="identicon">
+        <label class="gravitylabel" for="identicon"></label>
+        <!--Thanks to jdenticon.com for the identicon image generation!-->
+        <!--My identicon seed is the sha256 hash of the plain text word "JamieOnUbuntu".-->
+        <hr>
+        <div class="centertext sideitems">
+            <div class="sideitem">
+                <a href="https://github.com/JamieOnUbuntu" target="_blank" rel="noopener"><img src="/images/fontawesome/github.svg"></a>
+                <h4><a href="https://github.com/JamieOnUbuntu" target="_blank" rel="noopener">GitHub</a></h4>
+            </div>
+            <div class="sideitem">
+                <a href="https://twitter.com/JamieOnUbuntu" target="_blank" rel="noopener"><img src="/images/fontawesome/tw.svg"></a>
+                <h4><a href="https://twitter.com/JamieOnUbuntu" target="_blank" rel="noopener">Twitter</a></h4>
+            </div>
+            <div class="sideitem">
+                <a href="https://www.youtube.com/jamie90437x" target="_blank" rel="noopener"><img src="/images/fontawesome/yt.svg"></a>
+                <h4><a href="https://www.youtube.com/jamie90437x" target="_blank" rel="noopener">YouTube</a></h4>
+            </div>
+            <div class="sideitem">
+                <a href="https://keybase.io/JamieOnUbuntu" target="_blank" rel="noopener"><img src="/images/fontawesome/id-card.svg"></a>
+                <h4><a href="https://keybase.io/JamieOnUbuntu" target="_blank" rel="noopener">Keybase</a></h4>
+            </div>
+            <div class="sideitem">
+                <a href="/jamie-scaife.asc" target="_blank"><img src="/images/fontawesome/key.svg"></a>
+                <h4><a href="/jamie-scaife.asc" target="_blank">PGP</a></h4>
+            </div>
+            <div class="sideitem">
+                <a href="/rss.xml" target="_blank"><img src="/images/fontawesome/rss.svg"></a>
+                <h4><a href="/rss.xml" target="_blank">RSS</a></h4>
+            </div>
+        </div>
+        <hr>
+        <h2 class="centertext">Top Posts</h2>
+        <div class="redlink tops">
+<?php bloglist("recents"); ?>
+        </div>
+    </div>
+</div>
+
+<?php include "footer.php" ?>
+
+</body>
+
+</html>

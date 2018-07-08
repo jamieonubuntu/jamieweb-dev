4c838fbec2c8f93872f5cfa444b7eed5c7cf7ca1
Jamie Scaife <10353998+jamieweb@users.noreply.github.com>
2018-07-08 02:44:28 +0100

    Working on new footer

---
+++ footer.php
@@ -0,0 +1,62 @@
+<footer>
+    <div class="footer">
+        <div class="footer-column">
+            <h2><a href="/">JamieWeb</a></h2>
+            <p>Copyright &copy; Jamie Scaife 2018</p>
+            <p>Powered by Linux<span class="penguin">&#x1f427;</span></p>
+        </div>
+        <div class="footer-column footer-middle">
+            <h2>Links</h2>
+            <a href="https://github.com/jamieweb" target="_blank" rel="noopener">
+                <div class="footer-item">
+                    <img src="/images/fontawesome/github.svg">
+                    <h4>GitHub</h4>
+                </div>
+            </a>
+            <a href="https://twitter.com/jamieweb" target="_blank" rel="noopener">
+                <div class="footer-item">
+                    <img src="/images/fontawesome/tw.svg">
+                    <h4>Twitter</h4>
+                </div>
+            </a>
+            <a href="https://www.youtube.com/jamie90437x" target="_blank" rel="noopener">
+                <div class="footer-item">
+                    <img src="/images/fontawesome/yt.svg">
+                    <h4>YouTube</h4>
+                </div>
+            </a>
+            <a href="https://keybase.io/jamieweb" target="_blank" rel="noopener">
+                <div class="footer-item">
+                    <img src="/images/fontawesome/id-card.svg">
+                    <h4>Keybase</h4>
+                </div>
+            </a>
+            <a href="https://hackerone.com/jamieweb" target="_blank" rel="noopener">
+                <div class="footer-item">
+                    <img class="h1" src="/images/hackerone.png">
+                    <h4>HackerOne</h4>
+                </div>
+            </a>
+            <a href="/rss.xml" target="_blank">
+                <div class="footer-item">
+                    <img width="24px" src="/images/fontawesome/rss.svg">
+                    <h4>RSS Feed</h4>
+                </div>
+            </a>
+        </div>
+        <div class="footer-column">
+            <h2>Development</h2>
+        </div>
+    </div>
+</footer>
+
+
+
+
+
+<!--
+    <h5 class="footer-content two-mar-bottom">Copyright &copy Jamie Scaife 2018 | 
+
+
+    <h5 class="two-mar-top">This work is licensed under a <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank" rel="noopener">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</h5>
+</footer>-->

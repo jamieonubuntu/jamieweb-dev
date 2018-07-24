792c3c517015fd68a44c063cbe3337497a7c36c1
Jamie Scaife <10353998+jamieweb@users.noreply.github.com>
2018-07-25 00:28:36 +0100

    Limited progress today... sidetracked by GRUB upgrade

--- blog/travis-ci-csp-violation-testing/index.php
+++ blog/travis-ci-csp-violation-testing/index.php
@@ -37,13 +37,18 @@
         <div class="width-450">
             <p class="no-mar-top"><a href="https://travis-ci.org/" target="_blank" rel="noopener">Travis-CI</a> is an online continuous integration service for projects hosted on GitHub.</p>
             <p>It allows you to automatically test your code after every commit. These tests, known as 'builds', could be checking that your code compiles successfully, checking dependencies, checking compatibility, or in this case - checking a website for CSP violation reports.</p>
-            <p class="no-mar-bottom">Travis-CI employs virtual machines on Google Compute Engine and containers on Amazon EC2. The GCE VMs provide you will full root access to a complete system, while the EC2 containers are faster to boot. Travis-CI is free for open-source projects.</p>
+            <p>Each build uses its own virtual machine on Google Compute Engine, or a container on Amazon EC2. The GCE VMs provide you will full root access to a complete system, while the EC2 containers are faster to boot.</p>
+            <p class="no-mar-bottom">Travis-CI is free for open-source projects.</p>
         </div>
         <div class="width-545 display-flex flex-align-center flex-justify-center">
-            <p>img here</p>
+            <img src="/travisci-full-colour.png" width="450px">
         </div>
     </div>
-    <p>.</p>
+    <h2 id="headless-chrome-crawler">Headless Chrome Crawler</h2>
+    <p><a href="https://github.com/yujiosaka/headless-chrome-crawler" target="_blank" rel="noopener">Headless Chrome Crawler</a> by <a href="https://github.com/yujiosaka" target="_blank" rel="noopener">yujiosaka</a> is a web crawler powered by Headless Chrome.</p>
+    <img class="radius-8" src="/blog/testing-your-csp-using-travis-ci-and-headless-chrome-crawler/headless-chrome-crawler-github.png" width="1000px">
+    <p>Headless Chrome is a feature of the Google Chrome/Chromium browsers that allow you to run them in a headless environment. This essentially means that you can utilise the full functionality of a desktop web browser, but in a command-line environment. It's essentially a 'no GUI' mode, and comes in extremely useful for automated testing, page rendering, etc.</p>
+    <p>The reason that this comes in so useful for a crawler is that it allows the crawler to see the website as though it is an actual user using a proper desktop web browser. Many websites these days use frameworks such as AngularJS or React, which means that much of the content on the screen is actually written to the DOM using JavaScript, rather than simply being raw HTML. This means that using tools such as <code>curl</code> or <code>wget</code> to crawl websites will often result in an incomplete output, as the JavaScript has not run to populate the page with its content.</p>
 </div>
 
 <?php include "footer.php" ?>
